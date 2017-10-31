fc362df813527eedda17796b09f92869279bca9b
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-10-31 14:58:19 +0100

    Remove other students' input.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿using DemoBranch.DancingStickMan;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace DemoBranch
 {
@@ -12,12 +7,7 @@ namespace DemoBranch
         static void Main(string[] args)
         {
             Console.WriteLine("Välkommen till Hello World-appen!");
-            AndreChabbePatrik.Write();
-            Fredrik.Speak();
-            Perly.Speak();
-            EsterBergstrom.Speak();
-            JesperAndreas.Speak();
-            DanceMan.DanceAway(2);
+            
             Console.ReadLine();
         }
     }
